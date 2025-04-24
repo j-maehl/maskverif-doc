afec296d66939d35ce5818767f9c2fcc483743ef
Benjamin Gregoire <Benjamin.Gregoire@inria.fr>
2025-04-24 06:29:04 +0200

    update default.nix

--- default.nix
+++ default.nix
@@ -4,6 +4,7 @@ stdenv.mkDerivation {
   name = "mask-verif";
   src = ./.;
   buildInputs = [ ]
-    ++ (with ocamlPackages; [ ocaml findlib menhir zarith merlin ocamlgraph])
+    ++ (with ocamlPackages; [ ocaml findlib menhir menhirLib zarith merlin ocamlgraph])
+    ++ [dune_3]
     ;
 }
